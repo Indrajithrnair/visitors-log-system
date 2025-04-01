06f9d82d207897f9354d34ced7b9e9e49bd9a0fa
Indrajithrnair <indrajithnair02@gmail.com>
2025-04-02 00:51:44 +0530

    Restructure project for simpler deployment

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set the Python path to include the current directory and the visitor_log directory
-export PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/visitor_log
-
-# Run gunicorn with the correct WSGI application
-exec gunicorn visitor_log.wsgi:application --bind 0.0.0.0:$PORT 
